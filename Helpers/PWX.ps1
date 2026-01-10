9257687a3fe9b0f0a8b0460a5e9e0831a033d2ba
Shroudmoa <mikeshroud10@gmail.com>
2026-01-10 23:36:32 +0100

    Update PWX.ps1

--- Helpers/PWX.ps1
+++ Helpers/PWX.ps1
@@ -23,60 +23,11 @@ do {
             Write-Host "Running JetFont.ps1..." -ForegroundColor Green
             iex (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Shroudmoa/ProjectB/refs/heads/main/Scripts/JetFont.ps1").Content
         }
-        "3" {
-            Write-Host "Running Terminal.ps1..." -ForegroundColor Green
-           Write-Host "V2 : Installing fastfetch configuration..."
-
-$Utf8NoBom = New-Object System.Text.UTF8Encoding($false)
-
-$UserName = $env:USERNAME
-$UserPath = "C:/Users/$UserName"
-
-$FastfetchDir = "$UserPath/.config/fastfetch"
-$AsciiPath    = "$FastfetchDir/ascii.txt"
-$ConfigPath   = "$FastfetchDir/config.jsonc"
-
-$ProfilePath = $profile.CurrentUserAllHosts
-$ProfileDir  = Split-Path $ProfilePath
-
-$ProfileURL = "https://raw.githubusercontent.com/Shroudmoa/ProjectB/refs/heads/main/CTRL%20%2B%20V/Powershell/Microsoft.PowerShell_profile.ps1"
-$AsciiURL   = "https://raw.githubusercontent.com/Shroudmoa/ProjectB/refs/heads/main/CTRL%20%2B%20V/Fastdetch/ascii.txt"
-$ConfigURL  = "https://raw.githubusercontent.com/Shroudmoa/ProjectB/refs/heads/main/CTRL%20%2B%20V/Fastdetch/config.jsonc"
-
-
-if (-not (Get-Command fastfetch -ErrorAction SilentlyContinue)) {
-    winget install fastfetch -e --accept-source-agreements --accept-package-agreements
+"3" {
+$code = (Invoke-WebRequest "https://raw.githubusercontent.com/Shroudmoa/ProjectB/refs/heads/main/Scripts/Terminal.ps1").Content -join "`n"
+    & ([ScriptBlock]::Create($code))
 }
 
-New-Item -ItemType Directory -Path $FastfetchDir -Force | Out-Null
-New-Item -ItemType Directory -Path $ProfileDir  -Force | Out-Null
-
-attrib +h "$UserPath/.config"
-
-
-if (-not (Test-Path $ProfilePath)) {
-    New-Item -Path $ProfilePath -ItemType File -Force | Out-Null
-}
-
-$AsciiContent = (Invoke-WebRequest $AsciiURL -UseBasicParsing).Content
-[System.IO.File]::WriteAllText($AsciiPath, $AsciiContent, $Utf8NoBom)
-
-
-$ConfigContent = (Invoke-WebRequest $ConfigURL -UseBasicParsing).Content
-$ConfigContent = $ConfigContent -replace 'C:/Users/%USERPROFILE%', $UserPath
-$ConfigContent = $ConfigContent -replace '"source":\s*".*ascii.txt"', '"source": "' + $UserPath + '/.config/fastfetch/ascii.txt"'
-[System.IO.File]::WriteAllText($ConfigPath, $ConfigContent, $Utf8NoBom)
-
-
-$ProfileContent = (Invoke-WebRequest $ProfileURL -UseBasicParsing).Content
-$ProfileContent = $ProfileContent -replace 'C:/Users/%USERPROFILE%', $UserPath
-$ProfileContent = $ProfileContent -replace 'fastfetch\s+-c\s+".*config.jsonc"', 'fastfetch -c "' + $UserPath + '/.config/fastfetch/config.jsonc"'
-[System.IO.File]::WriteAllText($ProfilePath, $ProfileContent, $Utf8NoBom)
-
-Clear-Host
-Write-Host "Installation completed. Restart PowerShell^^"
-
-        }
         "4" {
             Write-Host "Running TerminalTH.ps1..." -ForegroundColor Green
             iex (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Shroudmoa/ProjectB/refs/heads/main/Scripts/TerminalTH.ps1").Content
@@ -147,6 +98,7 @@ Write-Host "Installation completed. Restart PowerShell^^"
     Write-Host ""
     Read-Host "Press Enter to return to the menu"
 } while ($true)
+
 
 
 
