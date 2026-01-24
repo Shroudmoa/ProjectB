eae2f26a54186848aa808c8150e16e7b14985669
Shroudmoa <mikeshroud10@gmail.com>
2026-01-24 18:06:20 +0100

    Add script to download and set random wallpaper
    This script downloads a random wallpaper from a specified URL and sets it as the desktop wallpaper.

---
+++ Scripts/WaL.ps1
@@ -0,0 +1,29 @@
+
+$baseUrl = "https://raw.githubusercontent.com/Shroudmoa/ProjectB/main/Wallpapers"
+
+
+$rand = Get-Random -Minimum 1 -Maximum 6  
+$randomImage = "$rand.jpg"
+
+
+$downloadUrl = "$baseUrl/$randomImage"
+
+
+$tempPath = "$env:TEMP\$randomImage"
+
+
+Invoke-WebRequest -Uri $downloadUrl -OutFile $tempPath
+
+
+Add-Type @"
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+
+
+[Wallpaper]::SystemParametersInfo(20, 0, $tempPath, 3)
+
+Write-Output "Wallpaper gesetzt: $randomImage"
